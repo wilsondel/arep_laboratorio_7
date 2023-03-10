7c2684aa9998649ab46cb52a466d3270ae247a07
unknown <wilson.delgado@labinfo.is.escuelaing.edu.co>
2023-03-10 17:31:41 -0500

    structure

--- src/main/java/org/example/SparkWebServer.java
+++ src/main/java/org/example/SparkWebServer.java
@@ -7,7 +7,14 @@ public class SparkWebServer {
     public static void main(String... args){
         staticFiles.location("/public");
         port(getPort());
+
+        String[] url = new String[]{"http://107.20.124.123:4568/json","http://34.229.254.99:4568/json","http://54.167.90.176:4568/json"};
+
         get("main", (req,res) -> "<h1>This is main!</h1>");
+//        post("api", (req,res) -> {
+//            HttpConnection.getAPIInfo("")
+//
+//        });
     }
 
     private static int getPort() {
